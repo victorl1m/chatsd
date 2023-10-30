6bed1f4fdde0a7569ca3125ae76be3a86523375a
Victor Lima <victor.lima@callink.com.br>
2023-10-30 11:20:29 -0300

    workflow.log

--- lhc_web/settings/settings.ini.php
+++ lhc_web/settings/settings.ini.php
@@ -10,8 +10,7 @@
       'theme' => 'defaulttheme',
       'installed' => true,
       'secrethash' => '1922AE810D613248DE9AAB3FC24C07C49C4FF87E6CB010FEF8D2A23FA28D4197',
-      'encryption_key' => 'db8dffbc8043c92b343cfe7497e0087d1afae9d923beef55ae8a6e3a6a87c8a7',
-      'debug_output' => true,
+      'debug_output' => false,
       'log_slow_request' => false,
       'templatecache' => false,
       'templatecompile' => false,
@@ -29,9 +28,10 @@
       'maps_api_key' => false,
       'default_group' => '',
       'default_user' => '',
-      'site_address' => 'http://localhost/',
+      'site_address' => 'http://10.33.4.234/',
       'trusted_host_patterns' => 
       array (
+        0 => '^.+\\.callink\\.com\\.br$',
       ),
       'extensions' => 
       array (
